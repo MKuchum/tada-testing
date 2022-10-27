e356bf1b43c2cc4719443cf80fd6d34663c494fe
MKuchum <s19b1_kuchumov@179.ru>
2022-10-28 01:56:26 +0300

    add flags

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/MKuchum/tada-testing/internal/web"
 	"net/http"
 )
 
 func main() {
-	s, err := web.NewServer("localhost:6379", "", 0)
+	var redisAddr string
+	var redisPassword string
+	var redisDB int
+	var serverAddr string
+	flag.StringVar(&redisAddr, "redis_addr", "localhost:6379", "Redis address in format 'host:port', by default 'localhost:6739'")
+	flag.StringVar(&redisPassword, "redis_password", "", "Redis password, by default empty")
+	flag.IntVar(&redisDB, "redis_db", 0, "Redis database, by default 0 (redis default DB)")
+	flag.StringVar(&serverAddr, "addr", "localhost:8080", "Server address in format 'host:port', by default 'localhost:8080'")
+	flag.Parse()
+
+	s, err := web.NewServer(redisAddr, redisPassword, redisDB)
 	if err != nil {
 		panic(fmt.Errorf("can not create server: %v", err))
 	}
-	if err := http.ListenAndServe("localhost:8080", s); err != nil {
+	if err := http.ListenAndServe(serverAddr, s); err != nil {
 		panic(fmt.Errorf("internal error while serve: %v", err))
 	}
 }
